4fbb8148988296df49bc48e6adc05a3b1e86094f
Billie Zhang <billiecoder@gmail.com>
2015-04-21 21:58:44 +0800

    order page style

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,12 @@
 class OrdersController < ApplicationController
   def new
+    @course = Course.find(params[:course_id]) if params[:course_id].present?
     @order = Order.new
+    @trade_no = Time.now.to_i.to_s
+  end
+
+  def create
+    redirect_to :root
   end
 
   private
