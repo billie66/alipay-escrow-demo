4515e1866c9768f32e7b9af99beeef7a482cdc2b
Peter Wang <happypeter1983@gmail.com>
2015-06-18 11:58:22 +0800

    fatal route error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :cups
   get "/orders/done" => "orders#done"
   post "/orders/alipay_notify" => "orders#alipay_notify"
+  resources :orders
+  resources :cups
   root 'cups#index'
 end
